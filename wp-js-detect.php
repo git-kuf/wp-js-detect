2b36793891885b7dc2ada0815295953c694fea51
Alex <kuflievskiy@gmail.com>
2013-09-05 16:44:42 -0400

    plugin option has been added

--- wp-js-detect.php
+++ wp-js-detect.php
@@ -29,9 +29,12 @@ License: GPLv3 license
 /**
  * Interface JsDetectInterface
  *
-*/
+ */
 interface JsDetectInterface{
     public function __construct();
+	public function install();
+	public function uninstall();	
+	public function plugin_settings();
     public function wp_non_js_notification();
 }
 
@@ -49,8 +52,77 @@ class JsDetect implements JsDetectInterface
     public function __construct()
     {
         add_action('wp_head', array($this,'wp_non_js_notification'));
+		add_action('admin_menu', array($this,'admin_menu_customization'));
+
+		register_activation_hook( __FILE__, array( $this, 'install' ) );
+		register_deactivation_hook( __FILE__, array( $this, 'uninstall' ) );		
     }
 
+    /**
+     * Function install
+     * This public function is used to add plugin option.
+     * 
+     * @param	-
+     * @return	-
+     * 
+     */    		
+    public function install()
+    {
+		$wp_non_js_notification_text = __('For full functionality of this site it is necessary to enable JavaScript. Here are the <a href="http://www.enable-javascript.com/" target="_blank"> instructions how to enable JavaScript in your web browser</a>.'); 								
+		add_option( 'wp_non_js_notification_text', $wp_non_js_notification_text, '', 'yes' );
+	}	    
+	
+    /**
+     * Function uninstall
+     * This public function is used to remove plugin options.
+     * 
+     * @param	-
+     * @return	-
+     * 
+     */    		
+    public function uninstall()
+    {
+		delete_option( 'wp_non_js_notification_text' );
+	}	    
+		
+    /**
+     * Function admin_menu_customization
+     * This public function is used for
+     * 
+     * @param
+     * @return
+     * 
+     */    
+    public function admin_menu_customization(){
+        add_menu_page('Js Detect', 'Js Detect', 'administrator', 'js-detect-settings',  array($this,'plugin_settings'),'', 99999999 );
+    }
+
+    /**
+     * Function plugin_settings
+     * This public function is used to update plugin settings.
+     * 
+     * @param	-
+     * @return	-
+     * 
+     */    	
+    public function plugin_settings(){
+	
+		if(isset($_POST['wp_non_js_notification_text'])){
+			update_option('wp_non_js_notification_text',$_POST['wp_non_js_notification_text']);
+		}
+		?>
+		<div class="wrap">
+			<div id="icon-tools" class="icon32"><br /></div><h2>Js Detect Settings</h2>		
+				<form action="#" method="post">
+					<label for="wp_non_js_notification_text">No Js Notification:</label>
+					<textarea rows="10" class="large-text code" id="wp_non_js_notification_text" name="wp_non_js_notification_text"><?php echo get_option('wp_non_js_notification_text'); ?></textarea>
+					<input type="submit" value="<?php _e('Update'); ?>" class="button button-primary button-large">
+				</form>
+			</div>
+		</div>
+		<?php
+	}
+	
     /**
      *  Function wp_non_js_notification
      *  This function is used to output the notification.
@@ -60,23 +132,14 @@ class JsDetect implements JsDetectInterface
     {
     	$plugin_path=get_bloginfo('url').'/wp-content/plugins/wp-js-detect/';
     	?>
-            <div id="jsDisabled">
-                <p>
-                     <?php 
-                            _e('For full functionality of this site it is necessary to enable JavaScript.
-                                Here are the <a href="http://www.enable-javascript.com/" target="_blank"> 
-                                instructions how to enable JavaScript in your web browser</a>.'); 
-                     
-                     ?>    
-                </p>
-            </div>
-            <script language="javascript">
-                var item = document.getElementById('jsDisabled');
-                if (item.style.display === "block" || !item.style.display )
-                {
-                    item.style.display = 'none';
-                }
-            </script>        
+		<div id="jsDisabled"><p><?php echo get_option('wp_non_js_notification_text'); ?></p></div>
+		<script language="javascript">
+			var item = document.getElementById('jsDisabled');
+			if (item.style.display === "block" || !item.style.display )
+			{
+				item.style.display = 'none';
+			}
+		</script>        
     	<style>
     	/*no JS message*/
             #jsDisabled{
