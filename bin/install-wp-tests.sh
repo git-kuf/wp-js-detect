3791a4e0c01c52530509c63c38ded85b0349be8c
Aleksey Kuflievskiy <kuflievskiy@gmail.com>
2014-04-14 23:33:38 +0300

    test - attempt 5 (pear) : phpmd,phpcs etc

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -79,16 +79,16 @@ php_tools() {
     # php go-pear.phar
 
     # Installing PHP Pear without user prompt
-    spawn wget -O /tmp/go-pear.phar http://pear.php.net/go-pear.phar
+    wget -O /tmp/go-pear.phar http://pear.php.net/go-pear.phar
     expect eof
 
-    spawn php /tmp/go-pear.phar
+    php /tmp/go-pear.phar
 
     expect "1-11, 'all' or Enter to continue:"
     send "\r"
     expect eof
 
-    spawn rm /tmp/go-pear.phar
+    rm /tmp/go-pear.phar
 
     
 	## PHP_CodeSniffer    
