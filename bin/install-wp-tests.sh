7ea330cb0137ca826a2b7d7e6deeb09cd2bcdb96
Aleksey Kuflievskiy <kuflievskiy@gmail.com>
2014-04-15 13:14:02 +0300

    test - attempt 18

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -29,6 +29,9 @@ install_wp() {
 	tar --strip-components=1 -zxmf /tmp/wordpress.tar.gz -C $WP_CORE_DIR
 
 	wget -nv -O $WP_CORE_DIR/wp-content/db.php https://raw.github.com/markoheijnen/wp-mysqli/master/db.php
+
+	cd $WP_CORE_DIR/wp-content/plugins
+	svn co --quiet http://plugins.svn.wordpress.org/wp-js-detect
 }
 
 install_test_suite() {
