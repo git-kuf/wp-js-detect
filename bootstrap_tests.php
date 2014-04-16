f7715082e92cc624eaa6b55ca3489d5a691a95b1
Aleksey Kuflievskiy <kuflievskiy@gmail.com>
2014-04-16 17:20:03 +0300

    php unit bootstrap change

--- bootstrap_tests.php
+++ bootstrap_tests.php
@@ -1,29 +1,21 @@
 <?php
+ob_start();
 
-/**
- * Bootstrap the plugin unit testing environment. Customize 'active_plugins'
- * setting below to point to your main plugin file.
- *
- * Requires WordPress Unit Tests (http://unit-test.svn.wordpress.org/trunk/).
- *
- * @package wordpress-plugin-tests
- */
+//change this to your path
+$path = dirname( dirname( dirname( dirname( dirname( dirname( __FILE__ ) ) ) ) ) ) . '/tmp/wordpress-tests-lib/includes/bootstrap.php';
 
-// Add this plugin to WordPress for activation so it can be tested.
-
-$GLOBALS['wp_tests_options'] = array(
-    'active_plugins' => array( "wp-js-detect/wp-js-detect.php" ),
-);
-
-// If the wordpress-tests repo location has been customized (and specified
-// with WP_TESTS_DIR), use that location. This will most commonly be the case
-// when configured for use with Travis CI.
-
-// Otherwise, we'll just assume that this plugin is installed in the WordPress
-// SVN external checkout configured in the wordpress-tests repo.
-
-if( false !== getenv( 'WP_TESTS_DIR' ) ) {
-    require getenv( 'WP_TESTS_DIR' ) . '/bootstrap.php';
+if ( file_exists( $path ) ) {
+    $GLOBALS['wp_tests_options'] = array(
+        'active_plugins' => array(
+            'disable-password-change-email/index.php',
+            'google-universal-analytics/googleanalytics.php',
+            'nix-anti-spam-light/nix-antispam-light.php',
+            'theme-my-login/theme-my-login.php',
+            'tinymce-advanced/tinymce-advanced.php',
+            'wordpress-importer/wordpress-importer.php',
+        )
+    );
+    require_once $path;
 } else {
-    require dirname( dirname( dirname( dirname( dirname( __FILE__ ) ) ) ) ) . '/bootstrap.php';
+    exit( "Couldn't find  wordpress-tests-lib/bootstrap.php. Path : " . $path );
 }
